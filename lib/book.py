bf3ac32843af2ee5b982374e725156d7f63845ca
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-03-21 20:22:32 +0300

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, pages, price):
+        if not isinstance(pages, int):
+            raise ValueError("page_count must be an integer")
+        self.title = title
+        self.pages = pages
+        self.price = price
+        self.discount = 0
+
+    def set_discount(self, amount):
+        if 0 <= amount <= 100:
+            self.discount = amount
+        else:
+            raise ValueError("Discount must be between 0 and 100.")
+
+    def discounted_price(self):
+        return self.price * (1 - self.discount / 100)
+
+    def turn_page(self):
+        return "Flipping the page...wow, you read fast!"
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, price):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        self.brand = brand
+        self.size = size
+        self.price = price
+        self.discount = 0
+        self.condition = "New"
+
+    def set_discount(self, amount):
+        if 0 <= amount <= 100:
+            self.discount = amount
+        else:
+            raise ValueError("Discount must be between 0 and 100.")
+
+    @property
+    def discounted_price(self):
+        return self.price * (1 - self.discount / 100)
+
+    def cobble(self):
+        self.condition = "New"
+
+    def repair(self):
+        return "The shoe has been repaired."
